b9d3f4d60d87451e4be94834384af2a22bebdd60
Davis <davisguybaker@gmail.com>
2024-03-27 22:17:57 -0400

    wrote function to play one round

--- script.js
+++ script.js
@@ -23,6 +23,35 @@ function getComputerChoice() {
     //player input should be case insensitive, use toLowerCase
     //return not console.log
 
+//possible cases are
+    //computer has rock, player has paper => player wins
+    //computer has paper, player has rock => computer wins
+    //computer has paper, player has scissors => player wins
+    //computer has scissors, player has paper => computer wins
+    //computer has scissors, player has rock => player wins
+    //computer has rock, player has scissors => computer wins
+
+function playRound(playerSelection, computerSelection){
+    if(computerSelection === playerSelection){
+        return("It's a tie!");
+    }else if(computerSelection === "rock" && playerSelection === "paper"){
+        return("You win! Paper beats Rock!");
+    }else if(computerSelection === "paper" && playerSelection === "rock"){
+        return("You lose. Rock looses to Paper.");
+    }else if(computerSelection === "paper" && playerSelection === "scissors"){
+        return("You win! Scissors beats Paper.");
+    }else if(computerSelection === "scissors" && playerSelection === "paper"){
+        return("You lose. Paper looses to Scissors");
+    }else if(computerSelection === "scissors" && playerSelection === "rock"){
+        return("You win! Rock beats Scissors");
+    }else if(computerSelection === "rock" && playerSelection === "scissors"){
+        return("You lose. Scissors looses to Rock");
+    }
+}
+
+//get user input
+    //make sure to decase it
+    //pass into functions
 //playGame function
     //plays five rounds of the game
     //keeps track of the score in each one
